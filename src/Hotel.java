2e9d1ad6a376f38634710f877636d3d3171f9abd
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-05-28 23:08:04 -0500

    Metodo Valores null
    se creo un método para cambiar los valores a null y se mejoro errores muy pequeños

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,18 +1,19 @@
 // Clase Hotel
 public class Hotel {
     // Atributos
-    String nombre;
-    String duenio;
-    String categoria;
-    int anio;
-    int habitaciones;
-    boolean disponible24_7;
+    private String nombre;
+    private String duenio;
+    private String categoria;
+    private int anio;
+    private int habitaciones;
+    private Boolean disponible24_7;
 
-    // Constructor
+    // Constructor vacío
     public Hotel() {
     }
 
-    public Hotel(String nombre, String duenio, String categoria, int anio, int habitaciones, boolean disponible24_7) {
+    // Constructor completo
+    public Hotel(String nombre, String duenio, String categoria, int anio, int habitaciones, Boolean disponible24_7) {
         this.nombre = nombre;
         this.duenio = duenio;
         this.categoria = categoria;
@@ -61,38 +62,53 @@ public class Hotel {
         this.habitaciones = habitaciones;
     }
 
-    public boolean isDisponible24_7() {
+    public Boolean getDisponible24_7() {
         return disponible24_7;
     }
 
-    public void setDisponible24_7(boolean disponible24_7) {
+    public void setDisponible24_7(Boolean disponible24_7) {
         this.disponible24_7 = disponible24_7;
     }
 
     // Métodos personalizados
 
     // Método para modificar los atributos del hotel
-    public void modificarHotel(String nombre, String dueño, String categoría, int año, int habitaciones, boolean disponible24_7) {
+    public void modificarHotel(String nombre, String duenio, String categoria, int anio, int habitaciones, Boolean disponible24_7) {
         this.nombre = nombre;
-        this.duenio = dueño;
-        this.categoria = categoría;
-        this.anio = año;
+        this.duenio = duenio;
+        this.categoria = categoria;
+        this.anio = anio;
         this.habitaciones = habitaciones;
         this.disponible24_7 = disponible24_7;
+        System.out.println("------------------------------");
+        System.out.println("Modificación de hotel completa");
     }
 
     // Método para imprimir los detalles del hotel
     public void imprimirDetalles() {
+        System.out.println("------------------------------");
         System.out.println("Nombre: " + nombre);
         System.out.println("Dueño: " + duenio);
         System.out.println("Categoría: " + categoria);
         System.out.println("Año: " + anio);
         System.out.println("Habitaciones: " + habitaciones);
-        System.out.println("Disponible 24/7: " + (disponible24_7 ? "Sí" : "No"));
+        System.out.println("Disponible 24/7: " + (disponible24_7 != null ? (disponible24_7 ? "Sí" : "No") : "null"));
     }
 
     // Método para modificar la disponibilidad del hotel
-    public void cambiarDisponibilidad(boolean disponible24_7) {
+    public void cambiarDisponibilidad(Boolean disponible24_7) {
         this.disponible24_7 = disponible24_7;
+    }
+
+    // Método para establecer valores null
+    public void valoresNull() {
+        setNombre(null);
+        setDuenio(null);
+        setCategoria(null);
+        setHabitaciones(0);
+        setAnio(0);
+        setDisponible24_7(null);
+        System.out.println("------------------------------");
+        System.out.println("Los valores se cambiaron a null exitosamente");
     }
 }
