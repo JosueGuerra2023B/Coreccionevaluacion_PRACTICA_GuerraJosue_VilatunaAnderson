26c471cc44a8f9d47323ddf4bc2632a9cc5131c6
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-05-28 22:02:06 -0500

    Modificación valores quemados

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // Creación de 5 instancias de la clase Hotel
-        Hotel hotel1 = new Hotel();
-        hotel1.setNombre("Hotel Paradise");
-        hotel1.setDuenio("Juan Pérez");
-        hotel1.setCategoria("5 estrellas");
-        hotel1.setAnio(2010);
-        hotel1.setHabitaciones(100);
-        hotel1.setDisponible24_7(true);
-
-        Hotel hotel2 = new Hotel();
-        hotel2.setNombre("Hotel Sunset");
-        hotel2.setDuenio("María López");
-        hotel2.setCategoria("4 estrellas");
-        hotel2.setAnio(2005);
-        hotel2.setHabitaciones(80);
-        hotel2.setDisponible24_7(false);
-
-        Hotel hotel3 = new Hotel();
-        hotel3.setNombre("Hotel Plaza");
-        hotel3.setDuenio("Pedro Gómez");
-        hotel3.setCategoria("3 estrellas");
-        hotel3.setAnio(2015);
-        hotel3.setHabitaciones(50);
-        hotel3.setDisponible24_7(true);
-
-        Hotel hotel4 = new Hotel();
-        hotel4.setNombre("Hotel Central");
-        hotel4.setDuenio("Ana Martínez");
-        hotel4.setCategoria("2 estrellas");
-        hotel4.setAnio(2000);
-        hotel4.setHabitaciones(30);
-        hotel4.setDisponible24_7(false);
-        Hotel hotel5 = new Hotel();
-        hotel5.setNombre("Hotel Beach");
-        hotel5.setDuenio("Carlos Sánchez");
-        hotel5.setCategoria("1 estrella");
-        hotel5.setAnio(2020);
-        hotel5.setHabitaciones(20);
-        hotel5.setDisponible24_7(true);
+        // Creación de 5 instancias de la clase Hotel con valores quemados
+        Hotel hotel1 = new Hotel("Hotel Paradise", "Juan Pérez", "5 estrellas", 2010, 100, true);
+        Hotel hotel2 = new Hotel("Hotel Sunset", "Maria Lópes", "4 estrellas", 2005, 80, false);
+        Hotel hotel3 = new Hotel("Hotel Plaza", "Pedro Gomes", "3 estrellas", 2015, 50, true);
+        Hotel hotel4 = new Hotel("Hotel Central", "Ana Martínez", "2 estrellas", 2000, 30, false);
+        Hotel hotel5 = new Hotel("Hotel Beach", "Carlos Sánchez", "1 estrella", 2020, 20, true);
 
         // Creación de dos objetos con valores nulos y posteriormente seteo de valores
         Hotel hotel6 = new Hotel();
@@ -70,6 +37,8 @@ public class Main {
         hotel7.imprimirDetalles();
         System.out.println("------------------------------");
         System.out.println("------------------------------");
-        System.out.println("Creado por Josué Guerra y Anderson Vilotuña");
+        System.out.println("Creado por Josué Guerra y Anderson Vilatuña");
+
+
     }
 }
